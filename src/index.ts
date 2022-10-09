609e735eb534efa3b9e0cc7f33f84e36e31fa2b6
Sergio Gonzalez Garrido <sgonzalez@jscode.es>
2022-10-09 22:54:39 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import core from './core'
 
-// Loading Node JS modules
-import path from 'path'
-
-// Module external
-import dotenv from 'dotenv'
-
-// Prepare file dotenv
-const fileEnv = '.env.dev'
-const pathEnv = path.resolve(__dirname, '..', fileEnv)
-
-// Initialize configuration
-dotenv.config({ path: pathEnv })
-
-
-const { CLIENT_ID, CLIENT_SECRET } = process.env
-
-// Pre testing
-const twichat = core.init({
-	client_id: CLIENT_ID,
-	client_secret: CLIENT_SECRET
-})
-
-twichat.on('ready', async () => {
-
-	console.log('ready')
-
-	const user = await twichat.getInfoUser('jscode_')
-
-	console.log(user)
-})
-
-
 export default core
